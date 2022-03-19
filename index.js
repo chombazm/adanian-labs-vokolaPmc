3b2a869beb3e0cbc56cc5493f86db626921efbbe
marksikaundi <emmanuelmark117@gmail.com>
2022-03-19 13:03:43 +0200

    merge new

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
+// import { BrowserRouter } from "react-router-dom";
+// import App from "./src/App";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./src/App";
 
 
-ReactDOM.render(<App />, document.getElementById("root"));
+    
+
+ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    document.getElementById("root")
+  );
